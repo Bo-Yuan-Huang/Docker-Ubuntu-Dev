5a3f9678a28884fe259a75b79d52348f7d430a80
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-12-14 15:40:40 -0500

    Move personalization to printed hint

--- personalize.sh
+++ personalize.sh
@@ -6,8 +6,9 @@ cp /app/vimrc ~/.vimrc
 cp /app/tmux.conf ~/.tmux.conf
 cp /app/cscope_maps.vim ~/.cscope_maps.vim
 cp -r /app/bash_it ~/.bash_it
-source ~/.bash_it/install.sh --silent
 
 git config --global user.name "Bo-Yuan Huang"
 git config --global user.email "byhuang1992@gmail.com"
 
+echo "$> source ~/.bash_it/install.sh --silent"
+echo "$> sudo dpkg-reconfigure tzdata"

1ea7e2852146678be9218dff44c5ff3053bd2b73
Bo-Yuan Huang <Bo-Yuan-Huang@users.noreply.github.com>
2018-12-17 15:02:07 -0500

    Update personalize.sh

--- user/personalize.sh
+++ user/personalize.sh
@@ -5,8 +5,6 @@
 cp /app/vimrc /home/byhuang/.vimrc
 cp /app/tmux.conf /home/byhuang/.tmux.conf
 cp /app/cscope_maps.vim /home/byhuang/.cscope_maps.vim
-cp -r /app/bash_it /home/byhuang/.bash_it
 
 git config --global user.name "Bo-Yuan Huang"
 git config --global user.email "byhuang1992@gmail.com"
-
